7eebdc8be0594fd2009cbfe401b2e7ba9f891733
ManojKumargithubacc <manoj.srinivasan@aspiresys.com>
2024-02-02 17:13:21 +0530

    02-Feb-2024

---
+++ practice.js
@@ -0,0 +1,8 @@
+let a="Manoj";
+console.log("Hi "+a);
+const b = "I am in ASPIRE";
+const c = b.replace("ASPIRE", "ASPIRE SYSTEMS");
+console.log(c);
+const arr = ["I", "am", "working", "in", "Aspire Systems"];
+const  arrstr= arr.join(" ");
+console.log(arrstr);
